9df809db14fd1c2d70f3a7476663a5a34b6bf063
park.seongbin <function.hola.b@joongang.co.kr>
2022-12-21 10:54:59 +0900

    :package: #2 Set up  prettier and jest

---
+++ Bowling/__test__/test.test.js
@@ -0,0 +1,50 @@
+const exp = require("constants");
+const isStrike = require("../src/js/index");
+
+describe("Bowling tests", () => {
+	test("테스트 코드의 설명", () => {
+		const test = 1;
+		expect(test).toBe(1);
+	});
+
+	test("1 is 1", () => {
+		expect(1).toBe(1);
+	});
+
+	test("abcd test", () => {
+		const test = "abcd";
+		expect(test).toEqual("abcd");
+	});
+
+	test("strike over 10 pin test", () => {
+		const strike = function (pin) {
+			if (pin === 10) {
+				return "X";
+			} else {
+				pin.toString();
+			}
+		};
+
+		const pin = 11;
+		const result = strike(pin);
+		expect(result).not.toBe("X");
+	});
+
+	test("IsStrike test", () => {
+		const IsStrike = function (pin) {
+			if (pin === 10) {
+				return true;
+			}
+			return false;
+		};
+
+		expect(IsStrike(10)).toBeTruthy();
+		expect(IsStrike(9)).toBeFalsy();
+		expect(IsStrike(0)).toBeFalsy();
+		expect(IsStrike(-10)).toBeFalsy();
+	});
+
+	test("module test", () => {
+		expect(isStrike(10)).toBeTruthy();
+	});
+});
--- Bowling/src/js/index.js
+++ Bowling/src/js/index.js
@@ -1,11 +1,8 @@
-
-
 function isStrike(pin) {
 	if (pin === 10) {
 		return true;
 	}
 	return false;
 }
-
 
 module.exports = isStrike;
