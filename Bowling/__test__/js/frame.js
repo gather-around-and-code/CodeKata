f3ad6407af32d5874aa451dd180ac25765019a25
h1d3r00t <tkskttk@gmail.com>
2023-01-03 23:57:40 +0900

    update

--- Bowling/__test__/js/frame.js
+++ Bowling/__test__/js/frame.js
@@ -1,8 +1,27 @@
 class Frame {
+    #_first
+    #_second
     #_totalScore
 
     constructor(first, second) {
+        this.#_first = first;
+        this.#_second = second;
         this.#_totalScore = first + second; 
+    }
+
+    isSpare() {
+        if(this.#_first + this.#_second === 10 && this.#_first != 10) {
+            return true
+        }
+        return false
+    }
+
+    isStrike() {
+        if(this.#_first === 10){
+            return true
+        }
+
+        return false
     }
 
     getTotalScore() {
